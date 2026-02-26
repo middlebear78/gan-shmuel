f242a285416dbf3a7c238f706ac27e2b79d52f9f
Davidshsh2 <davidshsh2@gmail.com>
2026-02-26 13:30:37 +0200

    remove views.py

--- billing/app.py
+++ billing/app.py
@@ -2,6 +2,14 @@ from flask import Flask
 from views import *
 
 app=Flask(__name__)
+# ____________________________________________________________
+# all routes
+
+# health check
+@app.route("/health")
+def health():
+    return "ok",200
+
 
 # ____________________________________________________________
 # named as __main__
--- billing/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-views=Flask(__name__)
-
-# health check
-@views.route("/health")
-def health():
-    return "ok",200
-
