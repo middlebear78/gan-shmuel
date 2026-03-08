3417a8bc1a77a2351ea4ce043bb1f0bd87c92039
Roi Aharoni <ah_roi@walla.co.il>
2026-03-08 17:26:49 +0200

    added test for Post/truck

--- billing/app.py
+++ billing/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from models import db, Provider, Truck, Rate
 from config import Config
 from routes.rates_routes import rates_bp
-
+from routes.truck_routes import truck_bp
 # routes Imports
 from routes.provider_routes import provider_bp
 from routes.healthCheck_route import health_bp
@@ -18,6 +18,8 @@ app.register_blueprint(health_bp)
 app.register_blueprint(provider_bp)
 
 app.register_blueprint(rates_bp)
+
+app.register_blueprint(truck_bp)
 # ____________________________________________________________
 # named as __main__
 if __name__=="__main__":
